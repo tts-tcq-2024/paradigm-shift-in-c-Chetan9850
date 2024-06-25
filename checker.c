b28885307c0ca7e2d260c8dc8c47db6c20500303
Chetan9850 <chetancdalawaimath@gmail.com>
2024-06-25 16:56:16 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isOutOfRange(float value, float lower_limit, float upper_limit, const char *parameter_name) {
-    if (value < lower_limit || value > upper_limit) {
-        printf("%s out of range!\n", parameter_name);
+int isTemperatureOutOfRange(float temperature) {
+    if (temperature < 0 || temperature > 45) {
+        printf("Temperature out of range!\n");
+        return 1;
+    }
+    return 0;
+}
+
+int isSocOutOfRange(float soc) {
+    if (soc < 20 || soc > 80) {
+        printf("State of Charge out of range!\n");
+        return 1;
+    }
+    return 0;
+}
+
+int isChargeRateOutOfRange(float chargeRate) {
+    if (chargeRate > 0.8) {
+        printf("Charge Rate out of range!\n");
         return 1;
     }
     return 0;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int temperatureOutOfRange = isOutOfRange(temperature, 0, 45, "Temperature");
-    int socOutOfRange = isOutOfRange(soc, 20, 80, "State of Charge");
-    int chargeRateOutOfRange = isOutOfRange(chargeRate, 0, 0.8, "Charge Rate");
+    int temperatureOutOfRange = isTemperatureOutOfRange(temperature);
+    int socOutOfRange = isSocOutOfRange(soc);
+    int chargeRateOutOfRange = isChargeRateOutOfRange(chargeRate);
 
     return !(temperatureOutOfRange || socOutOfRange || chargeRateOutOfRange);
 }
