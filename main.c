4b0da351aae0db10283e3c3e3e0e4157f3f9e5ec
Chetan9850 <chetancdalawaimath@gmail.com>
2024-08-04 22:31:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <assert.h>
-#include "battery_monitor.h"
+// #include <assert.h>
+// #include "battery_monitor.h"
 
-int main() {
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
-}
+// int main() {
+//     assert(batteryIsOk(25, 70, 0.7));
+//     assert(!batteryIsOk(50, 85, 0));
+// }
